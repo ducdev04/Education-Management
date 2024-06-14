d21616856990209e5b0f65a0b6fc01ea1da94c25
Duc <buiminhduc22082008@gmail.com>
2024-06-14 22:32:23 +0700

    create database

---
+++ DatabaseQLDT/CreateDB/src/CreateDB.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class CreateDB {
+    public static void main(String[] args) throws Exception {
+        try {
+            String url = "jdbc:mysql://localhost:3306/";
+
+            String databaseName = "Education_Management";
+            String userName = "root";
+            String password = "duc11092004";
+    
+            Connection connection = DriverManager.getConnection(url,userName, password);
+    
+            String sql = "CREATE DATABASE " + databaseName;
+    
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+            statement.close();
+            JOptionPane.showMessageDialog(null, databaseName + " Database has been created successfully", "System Message", JOptionPane.INFORMATION_MESSAGE);
+    
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
